434179dd9b223ccaf9c6050d36b851d79090cb71
Maysam Torabi <kherad@gmail.com>
2019-08-12 00:49:54 +0430

    Updates

---
+++ precache-manifest.d165edc65916182d46a0787fbdd5f664.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4320b5ea401af9d769c1ed26221a7de6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6344ffa73ccc3b3ff26b",
+    "url": "/static/css/2.954a4a9e.chunk.css"
+  },
+  {
+    "revision": "dee9ade9b61298f935aa",
+    "url": "/static/css/main.f0fa7b51.chunk.css"
+  },
+  {
+    "revision": "6344ffa73ccc3b3ff26b",
+    "url": "/static/js/2.1cac266d.chunk.js"
+  },
+  {
+    "revision": "dee9ade9b61298f935aa",
+    "url": "/static/js/main.edec37b7.chunk.js"
+  },
+  {
+    "revision": "143598380806021f1d4f",
+    "url": "/static/js/runtime~main.327b59dd.js"
+  }
+]);
