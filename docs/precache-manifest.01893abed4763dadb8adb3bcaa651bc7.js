42c7b0d53e63c6cfa5e7a681784c5bb3ac3b44de
Maysam Torabi <kherad@gmail.com>
2019-11-11 22:58:24 +0330

    building to docs

---
+++ docs/precache-manifest.01893abed4763dadb8adb3bcaa651bc7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e7646ef66fd33cea39ab036ae8d8412c",
+    "url": "/WeatherWidget/index.html"
+  },
+  {
+    "revision": "1784203adb586a2c1d0c",
+    "url": "/WeatherWidget/static/css/2.3c342dc0.chunk.css"
+  },
+  {
+    "revision": "a23f1a9e98cb92cea9bb",
+    "url": "/WeatherWidget/static/css/main.f0fa7b51.chunk.css"
+  },
+  {
+    "revision": "1784203adb586a2c1d0c",
+    "url": "/WeatherWidget/static/js/2.c4512224.chunk.js"
+  },
+  {
+    "revision": "a23f1a9e98cb92cea9bb",
+    "url": "/WeatherWidget/static/js/main.e4bf6192.chunk.js"
+  },
+  {
+    "revision": "d16ad42ef4dd39d97912",
+    "url": "/WeatherWidget/static/js/runtime~main.833a95f5.js"
+  }
+]);
